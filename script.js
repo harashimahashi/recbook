51257574e0e2db57a668003470b4fc738948cc34
harashimahashi <harashimahashi@gmail.com>
2016-10-18 14:57:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 document.addEventListener('DOMContentLoaded', function(){
+	// центрирование
 	var send = document.getElementById('sendAct');
-	send.style.left = document.body.clientWidth / 2 - send.offsetWidth / 2 + 'px';
+	var center = document.body.clientWidth / 2 - send.offsetWidth / 2;
+	send.style.left = center + 'px';
 	var sendChild = send.children;
-	var sendPos = send.getBoundingClientRect().left - send.offsetWidth / 1.5;
 	for(var i = 0; i < sendChild.length; i++){
-		sendChild[i].style.marginLeft = sendPos - send.clientWidth / 2 - sendChild[i].clientWidth / 2 + 'px';
+		sendChild[i].style.marginLeft = (send.clientWidth / 2 - 10) - sendChild[i].clientWidth / 2 + 'px';
 	}
+	//расширение текстового поля
 	var recipe = document.getElementById('recipe');
 	recipe.onmousemove = function(){
-		if(getComputedStyle(recipe).width > '227px'){
-			recipe.style.marginLeft = sendPos - send.clientWidth / 2 - recipe.clientWidth / 2 + 'px';
+		var rmarg = (send.clientWidth / 2 - 10) - recipe.clientWidth / 2;
+		if(getComputedStyle(recipe).width >= '233px'){
+			if(Math.sign(rmarg) == -1 || Math.sign(rmarg) == -0){
+				recipe.style.marginLeft = rmarg - 1.5 + 'px';
+			}
+			else if(Math.sign(rmarg) == 1 || Math.sign(rmarg) == 0){
+				recipe.style.marginLeft = rmarg + 'px';
+			}
 		}
 	}
 	recipe.addEventListener('mouseup', function(){
-		recipe.style.marginLeft = sendPos - send.clientWidth / 2 - recipe.clientWidth / 2 + 'px';
+		var rmarg = (send.clientWidth / 2 - 10) - recipe.clientWidth / 2;
+		if(getComputedStyle(recipe).width >= '233px'){
+			if(Math.sign(rmarg) == -1 || Math.sign(rmarg) == -0){
+				recipe.style.marginLeft = rmarg - 1.5 + 'px';
+			}
+			else if(Math.sign(rmarg) == 1 || Math.sign(rmarg) == 0){
+				recipe.style.marginLeft = rmarg + 'px';
+			}
+		}
 	});
+	//AJAX-запросы
+	var sendAct = document.forms.sendAct;
+	var load = sendAct.elements.load;
+	sendAct.onsubmit = function(e){
+		e.preventDefault();
+	}
+
+	load.onmousedown = function(){
+		
+		var formData = new FormData(document.forms.sendAct);
+	
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'filescript.php');
+		xhr.send(formData);
+	
+		sendAct.elements.recipe_name.value = '';
+		sendAct.elements.recipe.value = '';
+		document.getElementById('fileformlabel').innerHTML = '';
+	}
 });
